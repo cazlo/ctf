ebadd8a4df7e3676c595934a6be19c0821ac3135
cazlo <admin@apaettie.org>
2015-03-25 21:22:54 -0500

    Rearrange project structure

---
+++ ctf/src/ctf/agent/Agent007.java
@@ -0,0 +1,19 @@
+package ctf.agent;
+
+import ctf.common.AgentAction;
+import ctf.common.AgentEnvironment;
+
+import java.lang.Override;
+
+/**
+ * Created by Andrew Paettie on 3/25/15.
+ */
+public class Agent007 extends Agent {
+
+    @Override
+    public int getMove( AgentEnvironment inEnvironment ){
+        return AgentAction.DO_NOTHING;//todo
+    }
+
+
+}
